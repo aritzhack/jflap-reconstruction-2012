8d3af8dc7809bc0adead7eb65bf2520d7ed72df0
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-05-23 20:32:47 +0000

    Beginnings of controller structure. Will likely be changing.

---
+++ JFLAP8.0r/src/controller/ControllerMenuFactory.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javax.swing.JComponent;
+import javax.swing.JMenuBar;
+
+public class ControllerMenuFactory {
+
+	public static JMenuBar createMenuForView(JComponent primaryView) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
