0b85671c53c0aebd28a390bf849a9ab8e27474db
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-05-23 20:36:52 +0000

    Main files - these are the runnable classes. Do not work right now as View has not really be created.

---
+++ JFLAP8.0r/src/main/JFLAP.java
@@ -0,0 +1,26 @@
+package main;
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * This is the class that starts JFLAP.
+ * 
+ * @author Thomas Finley
+ * @author Moti Ben-Ari
+ *   All code moved to gui.Main
+ *   Parameter dontQuit false for command line invocation
+ */
+
+public class JFLAP {
+	public static void main(String[] args) {
+		main.Main.main(args, false);
+	}
+}
