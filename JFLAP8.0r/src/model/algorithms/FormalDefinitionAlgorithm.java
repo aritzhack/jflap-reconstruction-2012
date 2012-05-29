34fea35107ef6898d987f76b7575391d544e8332
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-05-29 16:40:33 +0000

    Added base class Formal definition Algorithm to accomadate those steppable algorithms which begin with an FD. This constitutes most of the steppable algorithms in JFLAP.

---
+++ JFLAP8.0r/src/model/algorithms/FormalDefinitionAlgorithm.java
@@ -0,0 +1,46 @@
+package model.algorithms;
+
+import debug.JFLAPDebug;
+import errors.BooleanWrapper;
+import model.formaldef.FormalDefinition;
+
+
+public abstract class FormalDefinitionAlgorithm<T extends FormalDefinition> extends SteppableAlgorithm {
+
+	private T myFormalDefinition;
+
+	public FormalDefinitionAlgorithm(T fd) {
+		myFormalDefinition = fd;
+		BooleanWrapper[] bw = fd.isComplete();
+		if (bw.length > 0){
+			throw new AlgorithmException(bw);
+		}
+		bw = checkOfProperForm(fd);
+		if (bw.length > 0)
+			throw new AlgorithmException(bw);
+		if (!this.reset())
+			throw new AlgorithmException("There an error occured with the initialization " +
+											"of the " + this.getDescriptionName() + ".");
+	}
+	
+	/**
+	 * Checks to see if the automaton to be converted is
+	 * of the proper form for this algorithm. <code>isComplete()</code>
+	 * has already been checked in the constructor.
+	 * 
+	 * @param fd
+	 * @return
+	 */
+	public abstract BooleanWrapper[] checkOfProperForm(T fd);
+
+	/**
+	 * Returns the formal definition object that this definition
+	 * was initialized with.
+	 * 
+	 * @return
+	 */
+	public T getOriginalDefinition(){
+		return myFormalDefinition;
+	}
+
+}
