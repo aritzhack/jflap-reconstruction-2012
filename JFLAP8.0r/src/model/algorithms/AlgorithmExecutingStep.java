4431f703237ee27b6608d63e12c92a735a58adb0
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-05-31 02:36:51 +0000

    Many adjustments made to various algorithms. Primarily this includes:       FA to regex - did heavy testing, it now seems to work for everything (of course this is not true)       Minimizer - tested in context of regex, fsa tranformations, held up to most

--- JFLAP8.0r/src/model/algorithms/AlgorithmExecutingStep.java
+++ JFLAP8.0r/src/model/algorithms/AlgorithmExecutingStep.java
@@ -1,5 +1,7 @@
 package model.algorithms;
 
+import model.algorithms.fsa.minimizer.MinimizeDFAAlgorithm;
+
 public abstract class AlgorithmExecutingStep<T extends SteppableAlgorithm> implements AlgorithmStep {
 
 	private T myAlg;
@@ -21,7 +23,22 @@ public abstract class AlgorithmExecutingStep<T extends SteppableAlgorithm> imple
 	@Override
 	public boolean execute() throws AlgorithmException {
 		myAlg = initializeAlgorithm();
-		return myAlg.stepToCompletion();
+		boolean completed = myAlg.stepToCompletion();
+		if (completed)
+			updateDataInMetaAlgorithm();
+		return completed;
+	}
+
+	/**	
+	 * does nothing by default, can be overridden to adjust meta 
+	 * data in the algorithm this class is implemented in.
+	 * 
+	 * @see MinimizeDFAAlgorithm
+	 * 
+	 */
+	public void updateDataInMetaAlgorithm(){
+		//does nothing by default, can be overridden to adjust meta 
+		//data in the algorithm this class is implemented in;
 	}
 
 	public abstract T initializeAlgorithm();
