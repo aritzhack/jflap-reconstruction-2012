567fdfd95d045eabacfb09feba29005f6af0eb8d
peggyli.224@gmail.com <peggyli.224@gmail.com@88abb661-d786-6c12-1558-74997d5da001>
2012-06-04 16:27:57 +0000

    Extracted addTerminalProduction method from the main parse method and added documentation with an example

--- trunk/JFLAP8.0r/src/model/grammar/parsing/cyk/CYKParser.java
+++ trunk/JFLAP8.0r/src/model/grammar/parsing/cyk/CYKParser.java
@@ -28,40 +28,15 @@ public class CYKParser extends Parser {
 
 		myProductions = g.getProductionSet();
 		myStartVariable = g.getStartVariable();
-
 	}
 
-	@Override
-	public String getDescriptionName() {
-		return "CYK Parser";
-	}
-
-	@Override
-	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object copy() {
-		return new CYKParser(getGrammar());
-	}
 
 	public boolean parse(SymbolString input) {
 		myTarget = input;
 		int length = input.size();
 		myTracer = new CYKParseTable(length);
 
-		// terminals
-		for (int i = 0; i < length; i++) {
-			SymbolString current = input.subList(i, i + 1);
-			for (Production p : myProductions) {
-				if (p.getRHS().equals(current)) {
-					CYKParseNode node = new CYKParseNode(p, i);
-					myTracer.addNode(i, i, node);
-				}
-			}
-		}
+		addTerminalProductions(input, length);
 
 		for (int increment = 1; increment < length; increment++) {
 			for (int row = 0; row < length - 1; row++) {
@@ -74,6 +49,38 @@ public class CYKParser extends Parser {
 		}
 		return myTracer.getLHSVariableSet(0, length - 1).contains(
 				myStartVariable);
+	}
+
+
+	/**
+	 * Adds the terminal productions for each terminal in the input string
+	 * to the CYK parse table
+	 * For the terminal <code>r</code> at index <code>i</code> of the input, 
+	 * for each production L -> r where L is the LHS variable deriving r,
+	 * a node representing L is added to row i, column i in the parse table
+	 * 
+	 * For example, for the input string <code>aabb</code> for the grammar
+	 * S -> AB | CB
+	 * C -> AS
+	 * A -> a
+	 * B -> b
+	 * for the language a^n b^n | n > 0
+	 * the parse table would hold an A node at [0, 0] and [1, 1]
+	 * and a B node at [2, 2] and [3, 3]
+	 * 
+	 * @param input - the entire string to be parsed
+	 * @param length - the length of the input string
+	 */
+	private void addTerminalProductions(SymbolString input, int length) {
+		for (int i = 0; i < length; i++) {
+			SymbolString current = input.subList(i, i + 1);
+			for (Production p : myProductions) {
+				if (p.getRHS().equals(current)) {
+					CYKParseNode node = new CYKParseNode(p, i);
+					myTracer.addNode(i, i, node);
+				}
+			}
+		}
 	}
 
 	// finds all productions whose RHS matches the substring
@@ -130,4 +137,27 @@ public class CYKParser extends Parser {
 		}
 		return false;
 	}
+
+
+	@Override
+	public String getDescriptionName() {
+		return "CYK Parser";
+	}
+
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Object copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
