8ecda56e9f349300a5b52fba6c1c79e0b5ef93a0
iancrossmc <iancrossmc@gmail.com>
2012-06-04 13:03:15 +0000

    Added working CYK-Parser (Doesn't have tracing yet)

--- JFLAP8.0r/src/model/grammar/parsing/cyk/CYKParser.java
+++ JFLAP8.0r/src/model/grammar/parsing/cyk/CYKParser.java
@@ -6,8 +6,6 @@ package model.grammar.parsing.cyk;
  */
 
 import java.util.*;
-
-import errors.BooleanWrapper;
 import model.formaldef.components.symbols.SymbolString;
 import model.formaldef.components.symbols.Variable;
 import model.grammar.Grammar;
@@ -18,7 +16,7 @@ import model.grammar.parsing.Parser;
 public class CYKParser extends Parser {
 	
 	private ProductionSet myProductions;
-	private static Variable myStartVariable;
+	private Variable myStartVariable;
 	
 	private ArrayList<Production> myDerivationRules;
 	
@@ -52,7 +50,6 @@ public class CYKParser extends Parser {
 		return null;
 	}
 
-	@Override
 	// should be parsing, not initialization
 	public boolean parse(SymbolString input) {
 		myParseTable = new CYKParseTable(input);
@@ -61,38 +58,28 @@ public class CYKParser extends Parser {
 		
 		// terminals
 		for (int i = 0; i < length; i++) {
-			int count = 0;
 			SymbolString current = input.subList(i, i+1);
 			for (Production p : myProductions) {
 				if (p.getRHS().equals(current)) {
 					myParseTable.addVariable(i, i, (Variable) p.getLHS().getFirst());
-					count++;
 				}
 			}
-			if (count == 0)		
-				return new BooleanWrapper(false, "At least one terminal in the string cannot be dervied.");
-			
 		}
 		
 		for (int increment = 1; increment < length; increment++) {
 			for (int row = 0; row < length - 1; row++) {
-				
 				int substringLength = row + increment;
 				if (length <= substringLength) {
 					break;
 				}
-				
 				findProductions(row, substringLength);
-				
 			}
 		}
-		
- 		return new BooleanWrapper(myParseTable.getVariableSet(0, length-1).contains(myStartVariable));
+ 		return myParseTable.getVariableSet(0, length-1).contains(myStartVariable);
 	}
 	
 	// finds all productions whose RHS matches the substring
 	private void findProductions (int start, int end) {
-		
 		for (int k = start; k < end; k++) {
 			for (Variable A : myParseTable.getVariableSet(start, k)) {
 				for (Variable B : myParseTable.getVariableSet(k+1, end)) {
@@ -105,8 +92,6 @@ public class CYKParser extends Parser {
 					}
 				}
 			}
-			
 		}
 	}
-
 }
