07606e2b58e7718e3370ed1783620acdcc3f8f9f
iancrossmc@gmail.com <iancrossmc@gmail.com@88abb661-d786-6c12-1558-74997d5da001>
2012-06-04 16:04:43 +0000

    Cleaned up CYK package, merging CYKParseTable with CYKTracer

--- trunk/JFLAP8.0r/src/model/grammar/parsing/cyk/CYKParseTable.java
+++ trunk/JFLAP8.0r/src/model/grammar/parsing/cyk/CYKParseTable.java
@@ -1,52 +1,40 @@
 package model.grammar.parsing.cyk;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import model.formaldef.components.symbols.SymbolString;
 import model.formaldef.components.symbols.Variable;
 
 public class CYKParseTable {
-
-	private Map<Integer, Map<Integer, Set<Variable>>> myParseTable;
+	private Map<Integer, Map<Integer, Set<CYKParseNode>>> myNodeMap;
 	
-	public CYKParseTable (int size) {
-		myParseTable = new HashMap<Integer, Map<Integer, Set<Variable>>>();
-		for (int i = 0; i < size; i++) {
-			HashMap<Integer, Set<Variable>> innerMap = new HashMap<Integer, Set<Variable>>();
+	public CYKParseTable(int size){
+		myNodeMap = new HashMap<Integer, Map<Integer, Set<CYKParseNode>>>();
+		for(int i=0; i<size;i++){
+			Map<Integer, Set<CYKParseNode>> innerMap = new HashMap<Integer, Set<CYKParseNode>>();
 			for (int j = i; j < size; j++) {
-				HashSet<Variable> innerSet = new HashSet<Variable>();
+				HashSet<CYKParseNode> innerSet = new HashSet<CYKParseNode>();
 				innerMap.put(j, innerSet);
 			}
-			myParseTable.put(i, innerMap);
+			myNodeMap.put(i, innerMap);
 		}
 	}
-
-	public Set<Variable> getVariableSet(int row, int col) {
-		return myParseTable.get(row).get(col);
-		
+	
+	public Set<CYKParseNode> getNodeSet(int row, int col){
+		return myNodeMap.get(row).get(col);
 	}
 	
-	/*
-	 * Warning: replaces ALL existing items in set with parameter vars contents
-	 */
-	public void putVariableSet(int row, int col, Set<Variable> vars) {
-		myParseTable.get(row).put(col, vars);
+	public Set<Variable> getLHSVariableSet(int row, int col){
+		Set<Variable> LHSVariables = new HashSet<Variable>();
+		for(CYKParseNode node : myNodeMap.get(row).get(col)){
+			LHSVariables.add(node.getLHS());
+		}
+		return LHSVariables;
 	}
 	
-	/*
-	 * Add a single variable to the specified location of the map
-	 * 
-	 * For example, addVariable(2, 3, A) would add the variable A to 
-	 * the third (0-based index for row 2) key of the parse table, and
-	 * then at the fourth (0-based index for col 3) key of the inner map
-	 * which is the value for row 2 of the outer map
-	 * in this way, the map can be considered like a 2-D array 
-	 */
-	public void addVariable(int row, int col, Variable v) {
-		myParseTable.get(row).get(col).add(v);
+	public void addNode(int row, int col, CYKParseNode node){
+		myNodeMap.get(row).get(col).add(node);
 	}
-	
-	
-	
-	
 }
--- trunk/JFLAP8.0r/src/model/grammar/parsing/cyk/CYKTester.java
+++ trunk/JFLAP8.0r/src/model/grammar/parsing/cyk/CYKTester.java
@@ -19,9 +19,20 @@
 
 package model.grammar.parsing.cyk;
 
-import jflap.model.grammar.Grammar;
-import jflap.model.grammar.Production;
-import jflap.model.grammar.UnrestrictedGrammar;
+import model.formaldef.components.symbols.Symbol;
+import model.formaldef.components.symbols.SymbolString;
+import model.formaldef.components.symbols.Terminal;
+import model.formaldef.components.symbols.Variable;
+import model.grammar.Grammar;
+import model.grammar.Production;
+import model.grammar.ProductionSet;
+import model.grammar.StartVariable;
+import model.grammar.TerminalAlphabet;
+import model.grammar.VariableAlphabet;
+
+//import jflap.model.grammar.Grammar;
+//import jflap.model.grammar.Production;
+//import jflap.model.grammar.UnrestrictedGrammar;
 
 /**
  * CYK Parser tester.
@@ -32,25 +43,49 @@ public class CYKTester {
 
 	public static void main(String[] args)
 	{
-		Production[] productions=new Production[10];
-		productions[0]=new Production("S","AD");
-		productions[1]=new Production("D","SC");
-		productions[2]=new Production("A","a");
-		productions[3]=new Production("C","b");
-		productions[4]=new Production("S","CB");
-		productions[5]=new Production("B","CE");
-		productions[6]=new Production("E","CB");
-		productions[7]=new Production("S","SS");
-		productions[8]=new Production("S","b");
-		productions[9]=new Production("B","CC");
+//		Production[] productions=new Production[10];
+//		productions[0]=new Production("S","AD");
+//		productions[1]=new Production("D","SC");
+//		productions[2]=new Production("A","a");
+//		productions[3]=new Production("C","b");
+//		productions[4]=new Production("S","CB");
+//		productions[5]=new Production("B","CE");
+//		productions[6]=new Production("E","CB");
+//		productions[7]=new Production("S","SS");
+//		productions[8]=new Production("S","b");
+//		productions[9]=new Production("B","CC");
+//		
+//		Grammar g=new UnrestrictedGrammar();
+//		g.addProductions(productions);
+//		g.setStartVariable("S");
+//		String target="abbbb";
+//		System.out.println("aa");
+//		CYKParserOld parser=new CYKParserOld(g);
+//		System.out.println(parser.solve(target));
+//		System.out.println("Trace = "+parser.getTrace());
 		
-		Grammar g=new UnrestrictedGrammar();
-		g.addProductions(productions);
-		g.setStartVariable("S");
-		String target="abbbb";
-		System.out.println("aa");
-		CYKParserOld parser=new CYKParserOld(g);
-		System.out.println(parser.solve(target));
-		System.out.println("Trace = "+parser.getTrace());
+		VariableAlphabet variables = new VariableAlphabet();
+		variables.addAll(new Symbol("S"), new Symbol("D"), new Symbol("A"), new Symbol("C"), new Symbol("B"));
+		TerminalAlphabet terminals = new TerminalAlphabet();
+		terminals.addAll(new Symbol("a"), new Symbol("b"));
+		ProductionSet functions = new ProductionSet();
+		functions.add(new Production(new Variable("S"), new SymbolString(new Variable("B"), new Variable("A"))));
+		functions.add(new Production(new Variable("S"), new SymbolString(new Terminal("a"))));
+		functions.add(new Production(new Variable("A"), new SymbolString(new Variable("B"), new Variable("D"))));
+		functions.add(new Production(new Variable("D"), new SymbolString(new Variable("A"), new Variable("C"))));
+		functions.add(new Production(new Variable("A"), new SymbolString(new Variable("A"), new Variable("A"))));
+		functions.add(new Production(new Variable("A"), new SymbolString(new Variable("B"), new Variable("C"))));
+		functions.add(new Production(new Variable("C"), new Terminal("b")));
+		functions.add(new Production(new Variable("A"), new Terminal("a")));
+		functions.add(new Production(new Variable("A"), new SymbolString(new Variable("B"), new Variable("A"))));
+		functions.add(new Production(new Variable("B"), new Terminal("a")));
+		StartVariable startVar = new StartVariable("S");
+		Grammar g = new Grammar(variables, terminals, functions, startVar);
+		CYKParser parser = new CYKParser(g);
+		Symbol a = new Terminal("a");
+		Symbol b = new Terminal("b");
+		SymbolString target = new SymbolString(a,a,b,a,b);
+		System.out.println(parser.parse(target));
+		System.out.println(parser.getTrace());
 	}
 }
