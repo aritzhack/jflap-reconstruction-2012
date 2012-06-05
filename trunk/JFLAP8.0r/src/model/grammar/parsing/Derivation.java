df7d589dee74f090f9a610522587f1a26c255f98
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-06-05 17:18:57 +0000

    Derivation class now does some stuff!

--- trunk/JFLAP8.0r/src/model/grammar/parsing/Derivation.java
+++ trunk/JFLAP8.0r/src/model/grammar/parsing/Derivation.java
@@ -1,29 +1,51 @@
 package model.grammar.parsing;
 
+import java.lang.Character.Subset;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+import model.formaldef.components.symbols.SymbolString;
 import model.grammar.Production;
 import model.util.UtilFunctions;
 
 public class Derivation {
 
 	private LinkedList<Production> myProductions;
-	private Integer[] mySubstitutions;
+	private LinkedList<Integer> mySubstitutions;
 	
-	public Derivation(Integer[] subs, Production ... productions) {
-		this();
-		myProductions.addAll(Arrays.asList(productions));
-		mySubstitutions = subs;
+	public Derivation(Production start) {
+		myProductions = new LinkedList<Production>();
+		mySubstitutions = new LinkedList<Integer>();
+		myProductions.add(start);
 	}
 	
-	public Derivation() {
-		myProductions = new LinkedList<Production>();
+	public void addAll(Production[] productions, Integer[] subs) {
+		if (productions.length != subs.length)
+			throw new ParserException("The number of productions and " +
+					"substituations in the derivation must be equal.");
+		for (int i = 0; i< productions.length; i++){
+			addStep(productions[i], subs[i]);
+		}
 	}
 
-	public boolean addProduction(Production p, int subIndex) {
-		mySubstitutions = UtilFunctions.combine(mySubstitutions, subIndex);
-		return myProductions.add(p);
+	public boolean addStep(Production p, int subIndex) {
+		return myProductions.add(p) && mySubstitutions.add(subIndex);
+	}
+	
+	public SymbolString createResult(){
+		SymbolString result = new SymbolString();
+		if (myProductions.isEmpty())
+		result.addAll(myProductions.getFirst().getRHS());
+		for (int i = 1; i < myProductions.size(); i++){
+			SymbolString sub = myProductions.get(i).getRHS();
+			result.replace(mySubstitutions.get(i-1), sub);
+		}
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return myProductions.toString();
 	}
 	
 }
