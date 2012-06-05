f1ee7a4e681e4259dc73e55d28ab8bf6c657d7ab
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-06-05 17:48:26 +0000

    multi-step return from derivation

--- trunk/JFLAP8.0r/src/model/grammar/parsing/Derivation.java
+++ trunk/JFLAP8.0r/src/model/grammar/parsing/Derivation.java
@@ -33,16 +33,39 @@ public class Derivation {
 	}
 	
 	public SymbolString createResult(){
+		return createResult(this.getLength());
+	}
+	
+	/**
+	 * Returns the number of productions in this derivation.
+	 * @return
+	 */
+	public int getLength() {
+		return myProductions.size();
+	}
+
+	public SymbolString createResult(int n){
 		SymbolString result = new SymbolString();
-		if (myProductions.isEmpty()){
-			
+		if (myProductions.size() < n){
+			throw new ParserException("This derivation does not have " + n +
+					"steps."	);
 		}
-		result.addAll(myProductions.getFirst().getRHS());
-		for (int i = 1; i < myProductions.size(); i++){
+		
+		if (n != 0)
+			result.addAll(myProductions.getFirst().getRHS());
+		for (int i = 1; i < n; i++){
 			SymbolString sub = myProductions.get(i).getRHS();
 			result.replace(mySubstitutions.get(i-1), sub);
 		}
 		return result;
+	}
+	
+	public SymbolString[] getResultArray(){
+		SymbolString[] steps = new SymbolString[this.getLength()-1];
+		for (int i = 1; i< steps.length; i++){
+			steps[i] = createResult(i);
+		}
+		return steps;
 	}
 	
 	@Override
