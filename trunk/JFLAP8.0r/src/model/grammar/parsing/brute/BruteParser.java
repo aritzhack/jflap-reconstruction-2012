56e291edc6221535dc587f31dda8091972d0cfb1
peggyli.224 <peggyli.224@88abb661-d786-6c12-1558-74997d5da001>
2012-06-06 18:57:05 +0000

    Refactoring of BruteParser

--- trunk/JFLAP8.0r/src/model/grammar/parsing/brute/BruteParser.java
+++ trunk/JFLAP8.0r/src/model/grammar/parsing/brute/BruteParser.java
@@ -1,7 +1,6 @@
 package model.grammar.parsing.brute;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -9,9 +8,7 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-import model.formaldef.components.symbols.Symbol;
 import model.formaldef.components.symbols.SymbolString;
-import model.formaldef.components.symbols.Variable;
 import model.grammar.Grammar;
 import model.grammar.Production;
 import model.grammar.parsing.Derivation;
@@ -20,19 +17,25 @@ import model.grammar.parsing.ParserException;
 import model.grammar.typetest.GrammarType;
 
 /**
+ * Brute force parser
+ * 
+ * Note: Brute force parser was re-implemented in Summer 2012
+ * to conform with the new parser hierarchy and new classes.
  * 
  * @author Peggy Li
+ * @author Julian Genkins
+ * @author Ian McMahon
  * 
  */
 
 public abstract class BruteParser extends Parser {
 
-	private int NODES_TO_GENERATE = 10000;
+	private static final int INCREMENT_CAPACITY_BY = 5000;
+	private int NODES_TO_GENERATE = 5000;
 
-	private Map<SymbolString, Set<SymbolString>> productionMap;
-	private Queue<Derivation> derivationsQueue;
-	private int nodesGenerated;
-	private Derivation answerDerivation;
+	private Queue<Derivation> myDerivationsQueue;
+	private int myNodesGenerated;
+	private Derivation myAnswerDerivation;
 
 	public BruteParser(Grammar g) {
 		super(g);
@@ -50,20 +53,16 @@ public abstract class BruteParser extends Parser {
 
 	@Override
 	public boolean resetParserStateOnly() {
-		nodesGenerated = 0;
-
-		createProductionMap();
+		myNodesGenerated = 0;
 		initializeQueue();
-
 		return true;
 	}
 
 	@Override
 	public boolean isAccept() {
-
-		// TODO Auto-generated method stub
-		for (Derivation d : derivationsQueue) {
+		for (Derivation d : myDerivationsQueue) {
 			if (d.createResult().equals(getInput())) {
+				myAnswerDerivation = d;
 				return true;
 			}
 		}
@@ -72,18 +71,21 @@ public abstract class BruteParser extends Parser {
 
 	@Override
 	public boolean isDone() {
-		// TODO Auto-generated method stub
-		return capacityReached() || isAccept() || derivationsQueue.isEmpty();
+		return capacityReached() || isAccept() || myDerivationsQueue.isEmpty();
 	}
 
 	@Override
 	public GrammarType getRequiredGrammarType() throws ParserException {
-		// TODO Auto-generated method stub
 		return GrammarType.UNRESTRICTED;
+	}
+	
+	@Override
+	public Derivation getDerivation () {
+		return myAnswerDerivation;
 	}
 
 	private boolean capacityReached() {
-		return nodesGenerated >= NODES_TO_GENERATE;
+		return myNodesGenerated >= NODES_TO_GENERATE;
 	}
 
 	@Override
@@ -93,56 +95,46 @@ public abstract class BruteParser extends Parser {
 		return true;
 	}
 
-	/**
-	 * Creates map where key = LHS and value = all RHS
-	 */
-	public void createProductionMap() {
-		if (productionMap != null)
-			return;
-
-		productionMap = new HashMap<SymbolString, Set<SymbolString>>();
-		for (Production p : getGrammar().getProductionSet()) {
-			SymbolString left = p.getLHS(), right = p.getRHS();
-			if (!productionMap.containsKey(left)) {
-				productionMap.put(left, new HashSet<SymbolString>());
-			}
-			productionMap.get(left).add(right);
-		}
-
-	}
+	
 
 	private void initializeQueue() {
-		derivationsQueue = new LinkedList<Derivation>();
+		myDerivationsQueue = new LinkedList<Derivation>();
 
 		SymbolString start = new SymbolString(getGrammar().getStartVariable());
 		Derivation d = new Derivation(new Production(new SymbolString(), start));
-			derivationsQueue.add(d);
-			nodesGenerated++;
-		}
-	
+		myDerivationsQueue.add(d);
+		myNodesGenerated++;
+	}
+
 
 	private boolean makeNextReplacement() {
 
 		// hold new Derivations, else queue not empty until end => cannot step
 		ArrayList<Derivation> temp = new ArrayList<Derivation>();
 
-		while (!derivationsQueue.isEmpty()) {
-			Derivation d = derivationsQueue.poll();
+		while (!myDerivationsQueue.isEmpty()) {
+			Derivation d = myDerivationsQueue.poll();
 			SymbolString result = d.createResult();
-			
-			for(int i=0; i<result.size();i++){
-				for(int j=i; j<result.size();j++){
-					SymbolString LHS = result.subList(i,j+1);
-					for(Production p : getGrammar().getProductionSet().getProductionsWithLHS(LHS)){
+
+			for (int i = 0; i < result.size(); i++) {
+				for (int j = i; j < result.size(); j++) {
+					SymbolString LHS = result.subList(i, j + 1);
+					for (Production p : getGrammar().getProductionSet()
+							.getProductionsWithLHS(LHS)) {
 						Derivation tempDerivation = d.copy();
 						tempDerivation.addStep(p, result.indexOf(LHS, i));
 						temp.add(tempDerivation);
-						nodesGenerated++;
+						myNodesGenerated++;
 					}
 				}
 			}
 		}
-		derivationsQueue.addAll(temp);
+		myDerivationsQueue.addAll(temp);
+		return true;
+	}
+
+	private boolean raiseCapacity() {
+		NODES_TO_GENERATE += INCREMENT_CAPACITY_BY;
 		return true;
 	}
 
