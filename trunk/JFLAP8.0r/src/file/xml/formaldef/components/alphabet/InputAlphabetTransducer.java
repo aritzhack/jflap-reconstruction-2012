5d23a28faea6e076a86f36ecafe8094449ed0e8e
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-06-09 14:41:30 +0000

    switched some packaging

---
+++ trunk/JFLAP8.0r/src/file/xml/formaldef/components/alphabet/InputAlphabetTransducer.java
@@ -0,0 +1,21 @@
+package file.xml.formaldef.components.alphabet;
+
+import model.automata.InputAlphabet;
+import model.formaldef.components.SetComponent;
+import model.formaldef.components.symbols.Symbol;
+
+public class InputAlphabetTransducer extends AlphabetTransducer{
+
+
+	@Override
+	public InputAlphabet createEmptyComponent() {
+		return new InputAlphabet();
+	}
+
+	@Override
+	public String getTag() {
+		return INPUT_ALPH;
+	}
+
+
+}
