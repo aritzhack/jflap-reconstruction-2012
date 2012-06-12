92c745f4d1ff4ca17c1706519790bb8523b443b2
peggyli.224 <peggyli.224@88abb661-d786-6c12-1558-74997d5da001>
2012-06-12 13:53:47 +0000

    Set package in the works

---
+++ JFLAP8.0r/src/model/languages/sets/InfiniteSet.java
@@ -0,0 +1,13 @@
+package model.languages.sets;
+
+/**
+ * 
+ * @author Peggy Li
+ *
+ */
+
+public abstract class InfiniteSet {
+	
+	
+
+}
---
+++ JFLAP8.0r/src/model/languages/sets/Main.java
@@ -0,0 +1,22 @@
+package model.languages.sets;
+
+import model.automata.InputAlphabet;
+import model.formaldef.components.alphabets.Alphabet;
+import model.formaldef.components.symbols.Symbol;
+import model.formaldef.components.symbols.SymbolString;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		Alphabet alpha = new InputAlphabet();
+		alpha.addAll(new Symbol("a"), new Symbol("ab"));
+		SymbolString a = SymbolString.createFromString("a", alpha);
+		SymbolString ab = SymbolString.createFromString("ab", alpha);
+		
+		System.out.println(a.getFirst().getString());
+		System.out.println(ab.getFirst().getString());
+		System.out.println(a.compareTo(ab));
+		
+	}
+
+}
