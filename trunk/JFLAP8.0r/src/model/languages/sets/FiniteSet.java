a627b8b5603cd4164f7b27aae9f0944052b9e6bc
peggyli.224 <peggyli.224@88abb661-d786-6c12-1558-74997d5da001>
2012-06-12 13:53:47 +0000

    Set package in the works

---
+++ trunk/JFLAP8.0r/src/model/languages/sets/FiniteSet.java
@@ -0,0 +1,18 @@
+package model.languages.sets;
+
+/**
+ * 
+ * @author Peggy Li
+ *
+ */
+
+public abstract class FiniteSet {
+
+	/**
+	 * Returns the cardinality of the set
+	 * 
+	 * @return number of elements in the set
+	 */
+	public abstract int getCardinality(); 
+	
+}
