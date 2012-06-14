614bbc883b8c6c8bcb74f935b38f2076ba9c420f
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2012-06-14 17:04:29 +0000

    Begin adding major changes re: listeners

--- JFLAP8.0r/src/model/formaldef/FormalDefinition.java
+++ JFLAP8.0r/src/model/formaldef/FormalDefinition.java
@@ -6,46 +6,85 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Observer;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import preferences.JFLAPPreferences;
 
-import oldnewstuff.util.Copyable;
+import util.Copyable;
 
 
 import model.JFLAPConstants;
-import model.formaldef.components.ChangeTypes;
-import model.formaldef.components.ComponentChangeEvent;
-import model.formaldef.components.ComponentChangeListener;
+import model.change.ChangeApplyingObject;
+import model.change.ChangeEvent;
+import model.change.ChangeListener;
+import model.change.ChangeTypes;
+import model.change.ChangeDistributingObject;
+import model.change.events.TrimAlphabetsEvent;
+import model.change.interactions.Interaction;
+import model.change.rules.applied.DefaultModeInUseRule;
+import model.change.rules.applied.DisallowedCharacterRule;
+import model.change.rules.applied.SelfIdenticalRule;
 import model.formaldef.components.FormalDefinitionComponent;
+import model.formaldef.components.SetComponent;
 import model.formaldef.components.alphabets.Alphabet;
 import model.formaldef.components.functionset.FunctionSet;
 import model.formaldef.components.symbols.Symbol;
-import model.formaldef.rules.applied.DisallowedCharacterRule;
+import model.undo.IUndoRedo;
 import errors.BooleanWrapper;
 
 
-public abstract class FormalDefinition extends ChangingObject implements Describable, 
-																			UsesSymbols, 
-																			ChangeListener, 
-																			ChangeTypes,
-																			JFLAPConstants,
-																			Copyable{
+public abstract class FormalDefinition extends ChangeApplyingObject implements Describable, 
+JFLAPConstants,
+ChangeTypes,
+UsesSymbols,
+Copyable{
 
 	private LinkedList<FormalDefinitionComponent> myComponents;
+//	private boolean amPurging;
+//	private PurgeEvent myPurgeEvent;
 
 
 	public FormalDefinition(FormalDefinitionComponent ... comps) {
 		myComponents = new LinkedList<FormalDefinitionComponent>();
 		for (FormalDefinitionComponent comp : comps){
 			myComponents.add(comp);
-			comp.addListener(this);
 		}
-		for (Alphabet a: this.getAlphabets())
-			a.addRules(new DisallowedCharacterRule(this));
+
+		updateRulesAndInteractions();
+	}
+
+	public void updateRulesAndInteractions() {
+		this.clearInteractionsAndRules();
+		if (JFLAPPreferences.isUserDefinedMode())
+			this.setUpUserDefinedRulesAndInteractions();
+		else
+			this.setUpDefaultRulesAndInteractions();
+		//add disallowedCharacterRule to all
+		for (Alphabet a: this.getAlphabets()){
+			a.addRules(new DisallowedCharacterRule(ITEM_ADD, this),
+						new DisallowedCharacterRule(ITEM_MODIFY, this));
+		}
+	}
+
+	public void setUpDefaultRulesAndInteractions(){
+		for (Alphabet a: this.getAlphabets()){
+			a.addRules(new DefaultModeInUseRule(ITEM_ADD, a, this),
+					new DefaultModeInUseRule(ITEM_REMOVE, a, this),
+					new DefaultModeInUseRule(ITEM_MODIFY, a, this));
+		}
+	}
+
+	public abstract void setUpUserDefinedRulesAndInteractions();
+
+	private void clearInteractionsAndRules() {
+		for (FormalDefinitionComponent comp: this.getComponents()){
+			comp.clearInteractions();
+			comp.clearRules();
+		}
 	}
 
 	public String toNtupleString(){
@@ -79,25 +118,48 @@ public abstract class FormalDefinition extends ChangingObject implements Describ
 		return null;
 	}
 
-	public void trimAlphabets(){
-		Set<Symbol> used = this.getUniqueSymbolsUsed();
-		for (Alphabet a: this.getAlphabets()){
-			a.retainAll(used);
+	public boolean trimAlphabets(){
+		TreeMap<Alphabet, Set<Symbol>> trimMap = new TreeMap<Alphabet, Set<Symbol>>();
+		for (Alphabet alph: this.getAlphabets()){
+			Set<Symbol> unused = new TreeSet<Symbol>(alph);
+			unused.removeAll(getUniqueSymbolsUsed(alph));
+			trimMap.put(alph, unused);
 		}
+		return this.applyChange(new TrimAlphabetsEvent(this, trimMap));
+	}
+	
+	/**
+	 * Retrieve the symbols used by this formal definition that apply
+	 * to the alphabet alph, i.e. that are actually in the alphabet.
+	 * This is used primarily for trimming.
+	 * 
+	 * @param alph
+	 * @return
+	 */
+	public abstract Set<? extends Symbol> getUniqueSymbolsUsed(Alphabet alph);
+	
+	@Override
+	public Set<Symbol> getUniqueSymbolsUsed() {
+		Set<Symbol> used = new TreeSet<Symbol>();
+		for (Alphabet a: this.getAlphabets()){
+			used.addAll(getUniqueSymbolsUsed(a));
+		}
+		return used;
 	}
 
-//	@Override
-//	public FormalDefinition copy() {
-//		ArrayList<FormalDefinitionComponent> cloned = new ArrayList<FormalDefinitionComponent>();
-//		for (FormalDefinitionComponent comp : this.getComponents())
-//			cloned.add(comp.copy());
-//		cloned.trimToSize();
-//		try {
-//			return (FormalDefinition) this.getClass().getConstructors()[0].newInstance(cloned.toArray());
-//		} catch (Exception e) {
-//			throw new RuntimeException(e);
-//		} 
-//	}
+
+	//	@Override
+	//	public FormalDefinition copy() {
+	//		ArrayList<FormalDefinitionComponent> cloned = new ArrayList<FormalDefinitionComponent>();
+	//		for (FormalDefinitionComponent comp : this.getComponents())
+	//			cloned.add(comp.copy());
+	//		cloned.trimToSize();
+	//		try {
+	//			return (FormalDefinition) this.getClass().getConstructors()[0].newInstance(cloned.toArray());
+	//		} catch (Exception e) {
+	//			throw new RuntimeException(e);
+	//		} 
+	//	}
 
 	public BooleanWrapper[] isComplete() {
 		ArrayList<BooleanWrapper> incomplete = new ArrayList<BooleanWrapper>();
@@ -147,23 +209,23 @@ public abstract class FormalDefinition extends ChangingObject implements Describ
 		return myComponents.toArray(new FormalDefinitionComponent[0]);
 	}
 
-	public Set<Symbol> getUnusedSymbols() {
-		Set<Symbol> symbols = this.getAllSymbolsInAlphabets();
-		symbols.removeAll(this.getUniqueSymbolsUsed());
-		return symbols;
-	}
+//	public Set<Symbol> getUnusedSymbols() {
+//		Set<Symbol> symbols = this.getAllSymbolsInAlphabets();
+//		symbols.removeAll(this.getUniqueSymbolsUsed());
+//		return symbols;
+//	}
 
-	@Override
-	public Set<Symbol> getUniqueSymbolsUsed() {
-		TreeSet<Symbol> used = new TreeSet<Symbol>();
-
-		for (FormalDefinitionComponent f: this.getComponents()){
-			if (f instanceof UsesSymbols)
-				used.addAll(((UsesSymbols) f).getUniqueSymbolsUsed());
-		}
-
-		return used;
-	}
+//	@Override
+//	public Set<Symbol> getUniqueSymbolsUsed() {
+//		TreeSet<Symbol> used = new TreeSet<Symbol>();
+//
+//		for (FormalDefinitionComponent f: this.getComponents()){
+//			if (f instanceof UsesSymbols)
+//				used.addAll(((UsesSymbols) f).getUniqueSymbolsUsed());
+//		}
+//
+//		return used;
+//	}
 
 	public Set<Symbol> getAllSymbolsInAlphabets() {
 		Set<Symbol> symbols = new HashSet<Symbol>();
@@ -173,34 +235,56 @@ public abstract class FormalDefinition extends ChangingObject implements Describ
 		return symbols;
 	}
 
-	@Override
-	public boolean purgeOfSymbol(Symbol s){
-		boolean result = false;
-		for (FormalDefinitionComponent f: this.getComponents()){
-			if (f instanceof UsesSymbols)
-				result = ((UsesSymbols) f).purgeOfSymbol(s) || result;
-		}
-		this.distributeChanged();
-		return result;
-	}
+//	@Override
+//	public void applyModification(Symbol from, Symbol to) {
+//		for (FormalDefinitionComponent f: this.getComponents()){
+//			if (f instanceof UsesSymbols)
+//				((UsesSymbols) f).applyModification(from, to);
+//		}
+//	}
+	
+//	@Override
+//	public boolean purgeOfSymbol(Alphabet a, Symbol s){
+//		boolean result = false;
+////		myPurgeEvent = new PurgeEvent(this, new RemoveEvent<Symbol>(a, s));
+////		amPurging = true;
+//		for (FormalDefinitionComponent f: this.getComponents()){
+//			if (f instanceof UsesSymbols)
+//				result = ((UsesSymbols) f).purgeOfSymbol(a, s) || result;
+//		}
+////		amPurging = false;
+////		this.distributeChange(myPurgeEvent);
+////		myPurgeEvent = null;
+//		return result;
+//	}
 
 	public abstract FormalDefinition alphabetAloneCopy();
 
-	@Override
-	public void stateChanged(ChangeEvent event) {
-		this.componentChanged((ComponentChangeEvent) event);
-	}
+//	@Override
+//	public void stateChanged(ChangeEvent event) {
+//		this.componentChanged(event);
+//	}
 
-	public void componentChanged(ComponentChangeEvent event){
-		for (Alphabet a: this.getAlphabets()){
-			if (event.comesFrom(a)){
-				switch (event.getType()){
-				case ITEM_REMOVED: 
-					this.purgeOfSymbol((Symbol) event.getArg(0));
-
-				}
-			}
-		}
-	}
+//	public void componentChanged(ChangeEvent event){
+//		if (amPurging && event instanceof IUndoRedo){
+//			myPurgeEvent.addSubEvents((IUndoRedo) event);
+//			return;
+//		}
+//		
+//		if (event instanceof ModifySymbolEvent){
+//			ModifySymbolEvent e = (ModifySymbolEvent) event;
+//			this.applyModification(e.getFrom(), e.getTo());
+//
+//		}
+//			
+//		if (event instanceof RemoveEvent){
+//			for (Alphabet alph: getAlphabets()){
+//				if (event.comesFrom(alph)){
+//					RemoveEvent<Symbol> e = (RemoveEvent<Symbol>) event;
+//					this.purgeOfSymbol(alph, e.getToRemove());
+//				}
+//			}
+//		}
+//	}
 
 }
