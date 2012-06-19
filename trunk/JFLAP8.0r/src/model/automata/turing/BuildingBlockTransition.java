811a53902adf80ead3bae3a3e83d5420c90ffd11
iancrossmc@gmail.com <iancrossmc@gmail.com@88abb661-d786-6c12-1558-74997d5da001>
2012-06-19 14:41:08 +0000

    Added BuildingBlock Turing machines

---
+++ trunk/JFLAP8.0r/src/model/automata/turing/BuildingBlockTransition.java
@@ -0,0 +1,44 @@
+package model.automata.turing;
+
+import java.util.Collection;
+
+import model.automata.SingleInputTransition;
+import model.automata.State;
+import model.automata.Transition;
+import model.formaldef.components.SetSubComponent;
+import model.formaldef.components.alphabets.Alphabet;
+import model.formaldef.components.symbols.Symbol;
+import model.formaldef.components.symbols.SymbolString;
+
+public class BuildingBlockTransition extends SingleInputTransition<BuildingBlockTransition> {
+	
+	public BuildingBlockTransition(BuildingBlock from, BuildingBlock to, SymbolString input){
+		super(from, to, input);
+	}
+
+	@Override
+	public String getDescriptionName() {
+		return "Building Block Transition";
+	}
+
+	@Override
+	public String getDescription() {
+		return "The transition for a Building Block Turing Machine";
+	}
+	
+	@Override
+	public BuildingBlock getFromState(){
+		return (BuildingBlock) super.getFromState();
+	}
+	
+	@Override
+	public BuildingBlock getToState(){
+		return (BuildingBlock) super.getToState();
+	}
+
+	@Override
+	public BuildingBlockTransition copy() {
+		return new BuildingBlockTransition(this.getFromState(), this.getToState(), new SymbolString(this.getInput()));
+	}
+
+}
