b2f9a291ce04663ea00d66892e6d670fbcb963e6
iancrossmc <iancrossmc@gmail.com>
2012-06-19 15:11:22 +0000

    Moved building blocks to their own package

---
+++ JFLAP8.0r/src/model/automata/turing/buildingblock/BuildingBlock.java
@@ -0,0 +1,48 @@
+package model.automata.turing.buildingblock;
+
+import java.util.Collection;
+import java.util.Set;
+
+import model.automata.State;
+import model.automata.turing.TuringMachine;
+import model.formaldef.UsesSymbols;
+import model.formaldef.components.alphabets.Alphabet;
+import model.formaldef.components.symbols.Symbol;
+
+public class BuildingBlock extends State implements UsesSymbols{
+	private TuringMachine myMachine;
+
+	public BuildingBlock(TuringMachine machine, String name, int id){
+		super(name, id);
+		myMachine = machine;
+	}
+	
+	public void setTuringMachine(TuringMachine tm){
+		myMachine = tm;
+	}
+	
+	public TuringMachine getTuringMachine(){
+		return myMachine;
+	}
+
+	@Override
+	public Set<Symbol> getSymbolsUsedForAlphabet(Alphabet a) {
+		return myMachine.getSymbolsUsedForAlphabet(a);
+	}
+
+	@Override
+	public boolean applySymbolMod(String from, String to) {
+		return myMachine.applySymbolMod(from, to);
+	}
+
+	@Override
+	public boolean purgeOfSymbols(Alphabet a, Collection<Symbol> s) {
+		return myMachine.purgeOfSymbols(a, s);
+	}
+	
+	@Override
+	public String toDetailedString(){
+		return super.toDetailedString()+myMachine.toString();
+	}
+	
+}
