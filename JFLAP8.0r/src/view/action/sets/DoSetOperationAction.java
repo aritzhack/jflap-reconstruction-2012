2560cb32c33b9f7944844d8c2276b3ecfcf9093e
peggyli.224 <peggyli.224@gmail.com>
2012-07-19 15:03:00 +0000

    Sets view updates

---
+++ JFLAP8.0r/src/view/action/sets/DoSetOperationAction.java
@@ -0,0 +1,38 @@
+package view.action.sets;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.AbstractAction;
+
+import model.sets.AbstractSet;
+import model.sets.SetsManager;
+import model.sets.operations.SetOperation;
+import model.sets.operations.SetOperationException;
+
+public class DoSetOperationAction extends AbstractAction {
+
+	private SetOperation myOperation;
+
+	public DoSetOperationAction(SetOperation operation) {
+		myOperation = operation;
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		ArrayList<AbstractSet> operands = SetsManager.ACTIVE_REGISTRY.getSelectedSets();
+		try {
+			myOperation.setOperands(operands);
+			AbstractSet answer = myOperation.evaluate();
+			SetsManager.ACTIVE_REGISTRY.add(answer);
+		} catch (SetOperationException e1) {
+			e1.printStackTrace();
+		}
+
+
+
+	}
+
+}
