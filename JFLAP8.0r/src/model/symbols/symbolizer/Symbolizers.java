d8ee1aab8f7029eb300e7252c6acd09decd015cf
jgenk90 <jgenk90@88abb661-d786-6c12-1558-74997d5da001>
2013-03-04 05:27:20 +0000

    Updates to: Graph structure and drawing in preparation for selection drawing and automaton editor development Formalization of mode - Design document in OneNote Symbolizing is clean, efficient, and effective now.

--- JFLAP8.0r/src/model/symbols/symbolizer/Symbolizers.java
+++ JFLAP8.0r/src/model/symbols/symbolizer/Symbolizers.java
@@ -1,6 +1,7 @@
 package model.symbols.symbolizer;
 
 import model.formaldef.FormalDefinition;
+import model.formaldef.components.alphabets.Alphabet;
 import model.symbols.SymbolString;
 
 
@@ -8,6 +9,11 @@ public class Symbolizers {
 
 	public static SymbolString symbolize(String in, FormalDefinition fd) {
 		Symbolizer s = new AdvancedSymbolizer(fd);
+		return s.symbolize(in);
+	}
+	
+	public static SymbolString symbolize(String in, Alphabet ... alphs) {
+		Symbolizer s = new SimpleSymbolizer(alphs);
 		return s.symbolize(in);
 	}
 
