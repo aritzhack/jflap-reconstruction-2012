54b5ffdfc06e0a9ea1675567ecd7dcb32278b7b4
iancrossmc@gmail.com <iancrossmc@gmail.com@88abb661-d786-6c12-1558-74997d5da001>
2013-06-07 23:25:00 +0000

    CTRL-o keybinding added. Also, in last commit, added display of filename when it changes to something else.

--- trunk/JFLAP8.0r/src/view/action/file/OpenAction.java
+++ trunk/JFLAP8.0r/src/view/action/file/OpenAction.java
@@ -1,10 +1,12 @@
 package view.action.file;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
+import javax.swing.KeyStroke;
 
 import universe.JFLAPUniverse;
 import universe.preferences.JFLAPPreferences;
@@ -16,6 +18,8 @@ public class OpenAction extends AbstractAction {
 
 	public OpenAction() {
 		super("Open...");
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O,
+				JFLAPConstants.MAIN_MENU_MASK));
 	}
 	
 	@Override
